d3ef5c4e7e9f9155592c2e4ba97bd7b76a33e3fb
Ahmad Hasan <ahmadsalih7@hotmail.com>
2024-05-14 10:51:17 +0300

    Problem 3 solved
    Working with strings

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+char[] f(char[] a, int start, int len)
+{
+    if (start < 0 || len < 0 || start + len > a.Length)
+    {
+        return null;
+    }
+    return new string(a).Substring(start, len).ToCharArray();
+}
+
+char[] chars = { 'a', 'b', 'c' };
+Console.WriteLine(f(chars, 1, 2));
+
